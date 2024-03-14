87867f1e8a97a24578e88729df80ea58c3362671
tothricsaj <tothricsaj@gmail.com>
2024-03-14 12:43:08 +0100

    feat: parsed commands and scenes name

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 use std::fs::File;
-use std::io::{self, BufRead, Read};
+use std::io::{ BufRead, BufReader };
 
 fn main() {
-  let mut file = match  File::open("src/test.story") {
+  let mut scenes:Vec<String> = vec![];
+  let mut commands:Vec<String> = vec![];
+
+  let file = match  File::open("src/test.story") {
     Err(why) => panic!("could not open the and the reason: {}", why),
-    Ok(file) => file,
+    Ok(file) => BufReader::new(file).lines(),
   };
 
-  let mut content = String::new();
-
-  file.read_to_string(&mut content).unwrap();
-
-  println!("{}", content);
+  for line in file.flatten() {
+    println!("{}", line);
+    let first_char:Option<char> = line.chars().nth(0);
+    if first_char == Some('@') {
+      scenes.push(line);
+    } else if first_char == Some('#') {
+      commands.push(line);
+    }
+  }
+  
+  println!("commands: {:?}", commands);
+  println!("scenes: {:?}", scenes);
 }
