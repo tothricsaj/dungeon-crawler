8b569c63e405651e79f09f1f05e5fab0604800fa
tothricsaj <tothricsaj@gmail.com>
2024-03-14 12:24:38 +0100

    feat: basic file read

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs::File;
+use std::io::{self, BufRead, Read};
+
 fn main() {
-    println!("Hello, world!");
+  let mut file = match  File::open("src/test.story") {
+    Err(why) => panic!("could not open the and the reason: {}", why),
+    Ok(file) => file,
+  };
+
+  let mut content = String::new();
+
+  file.read_to_string(&mut content).unwrap();
+
+  println!("{}", content);
 }
---
+++ src/test.story
@@ -0,0 +1,13 @@
+@Prelog
+This is the entrance of the dungeon. You see two way - left and right.
+Where do you go?
+#Left[LeftOption]
+#Right[RightOption]
+
+@LeftOption
+This is the left option.
+#GoBack(Prelog)
+
+@RightOption
+This is right. Nice
+#GoBack(Prelog)
