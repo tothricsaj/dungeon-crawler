42bc1bdd2742d5c47df452e39cb95c4ee47a2de9
tothricsaj <tothricsaj@gmail.com>
2024-03-18 17:25:27 +0100

    feat: added basic struct
    I want to make struct staff

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use std::fs::File;
 use std::io::{ BufRead, BufReader };
 
+struct Scene {
+  name: String,
+  story: Vec<String>,
+  commands: Vec<String>
+}
+
 fn main() {
   let mut scenes:Vec<String> = vec![];
+  let mut scene_names:Vec<String> = vec![];
   let mut commands:Vec<String> = vec![];
 
   let file = match  File::open("src/test.story") {
@@ -14,12 +21,15 @@ fn main() {
     println!("{}", line);
     let first_char:Option<char> = line.chars().nth(0);
     if first_char == Some('@') {
-      scenes.push(line);
+      scene_names.push(line);
     } else if first_char == Some('#') {
       commands.push(line);
+    } else {
+      scenes.push(line);
     }
   }
   
   println!("commands: {:?}", commands);
+  println!("scenes: {:?}", scene_names);
   println!("scenes: {:?}", scenes);
 }
--- src/test.story
+++ src/test.story
@@ -3,11 +3,11 @@ This is the entrance of the dungeon. You see two way - left and right.
 Where do you go?
 #Left[LeftOption]
 #Right[RightOption]
-
+&
 @LeftOption
 This is the left option.
 #GoBack(Prelog)
-
+&
 @RightOption
 This is right. Nice
 #GoBack(Prelog)
